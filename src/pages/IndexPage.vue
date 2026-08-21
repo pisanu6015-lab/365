d2ceaefe64b1c845bb2d27a145e546f992ba37fc
Nuker <Pisanu6015@gmail.com>
2026-08-21 15:20:19 +0700

    v.6-Button with Japanese language-365

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,12 +1,12 @@
 <template>
   <q-page class="q-pa-md">
     <q-form class="q-gutter-md" style="max-width: 600px">
-      <q-input filled label="Your name *" hint="Name and surname" />
-      <q-input filled type="number" label="Your age *" />
-      <q-toggle label="I accept the license and terms" />
+      <q-input filled label="Your name (ชื่อ-สกุล) *" hint="Name and surname" />
+      <q-input filled type="number" label="Your age (อายุ) *" />
+      <q-toggle label="I accept the license and terms (我接受许可和条款语言)" />
       <div>
-        <q-btn label="SUBMIT" type="submit" color="primary" />
-        <q-btn label="RESET" type="reset" color="primary" flat class="q-ml-sm" />
+        <q-btn label="SUBMIT (提出する)" type="submit" color="primary" />
+        <q-btn label="RESET (リセット)" type="reset" color="primary" flat class="q-ml-sm" />
       </div>
     </q-form>
   </q-page>
